09eefde73ab62dfb102acf07b426feb023de454a
Sweet1022 <40279725+Sweet1022@users.noreply.github.com>
2025-09-06 06:41:28 -0700

    Update ddl_gold.sql

--- scripts1/gold/ddl_gold.sql
+++ scripts1/gold/ddl_gold.sql
@@ -15,6 +15,64 @@ Usage:
 */
 
 -- =========================================================================
+--create a View for the Gld Dimension data
+--everything in Gold is as a View
+--now need system generated unique identifier to each record  Surrogate Key
+--use the Window Function ROW_NUMBER
+-- =========================================================================
+
+-- =========================================================================
+--Create Dimension:  gold.dim_customers
+-- =========================================================================
+If OBJECT_ID('gold.dim_customers', 'V') IS NOT NULL
+  DROP VIEW gold.dim_customers;
+GO
+
+CREATE VIEW gold.dim_customers AS
+SELECT 
+	ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key,
+	ci.cst_id AS customer_id, 
+	ci.cst_key AS customer_number, 
+	ci.cst_firstname AS first_name, 
+	ci.cst_lastname AS last_name,
+	la.cntry AS country,
+	ci.cst_material_status AS marital_status,
+	CASE WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr  --CRM is the Master for gender Info
+	ELSE COALESCE(ca.gen, 'n/a')
+	END AS new_gen,
+	ca.bdate AS birthdate, 
+	ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci 
+LEFT JOIN silver.erp_cust_az12 ca 
+ON ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 la 
+ON ci.cst_key = la.cid;
+
+-- =========================================================================
+--Create Dimension:  gold.dim_sales
+-- =========================================================================
+If OBJECT_ID('gold.dim_sales', 'V') IS NOT NULL
+  DROP VIEW gold.dim_sales;
+GO
+
+CREATE VIEW gold.fact_sales AS
+SELECT 
+  sd.sls_ord_num AS order_number,
+  pr.product_key,
+  cu.customer_key,
+  sd.sls_order_dt AS order_date,
+  sd.sls_ship_dt AS shipping_date,
+  sd.sls_due_dt AS due_date,
+  sd.sls_sales AS sales_amount,
+  sd.sls_quantity AS quantity,
+  sd.sls_price AS price
+FROM silver.crm_sales_details sd
+LEFT JOIN gold.dim_products pr
+  ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+  ON sd.sls_cust_id = cu.customer_id;
+
+-- =========================================================================
 --Create Dimension:  gold.dim_products
 -- =========================================================================
 If OBJECT_ID('gold.dim_products', 'V') IS NOT NULL
